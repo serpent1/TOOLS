e84afed8b5b00c90079f5476a9570ac1d5b4fb36
songsong.sheng <13166030896@163.com>
2018-12-26 21:21:48 +0800

    Add sqlitedb to store the Guid

--- CreateGUID/Form1.cs
+++ CreateGUID/Form1.cs
@@ -7,41 +7,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SQLite.CodeFirst;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace CreateGUID
 {
+
     public partial class GUID : Form
     {
+        private LogWriter logger;
+
         public GUID()
         {
-            InitializeComponent();
+            InitializeComponent();          
+            logger = new LogWriter("Logger");
+            logger.LogWrite("\r\n");
+            logger.LogWrite("\r\n");
+            logger.LogWrite("程序启动\r\n-------------------------------------------------------------");
         }
-
+        // public int i = 0;
+        /// <summary>
+        /// create a new GUID
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            string guid = Guid.NewGuid().ToString();
-            string GUID = guid.ToUpper();
-            textBox1.AppendText(GUID+ "\r\n");           
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {           
             try
             {
-                Clipboard.SetText(textBox1.Text);
+                string guid = Guid.NewGuid().ToString();
+                string GUID = guid.ToUpper();
+                logger.LogWrite($"生成新的GUID：{GUID}！");
+                textBox1.AppendText(GUID + "\r\n");
+                logger.LogWrite($"开始将GUID:{GUID}写入数据库！");
+                using (var db = new MyDbContext())
+                {
+                    db.Notes.Add(new Note { Text = GUID,date= DateTime.Now.ToLongDateString(),time = DateTime.Now.ToLongTimeString() });                  
+                    db.SaveChanges();
+                }
             }
             catch(Exception ex)
             {
-                System.Console.Write(ex);                         
-                StringBuilder messageBuilder = new StringBuilder(200);
-                messageBuilder.Append("请检查GUID生成框是否为空");                
-                MessageBox.Show(messageBuilder.ToString());               
-            }                  
+                logger.LogWrite(ex.Message);
+            }
         }
-
+        /// <summary>
+        /// copy GUID
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {  
+                logger.LogWrite("复制文本框内的内容！");
+                Clipboard.SetText(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                logger.LogWrite(ex.Message);
+                StringBuilder messageBuilder = new StringBuilder(200);
+                messageBuilder.Append("请检查GUID生成框是否为空！");
+                MessageBox.Show(messageBuilder.ToString());
+            }
+        }
+        /// <summary>
+        /// clear the box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
-        {           
-            textBox1.Clear();
+        {
+            try
+            {
+                logger.LogWrite("清空文本框！");
+                textBox1.Clear();
+            }
+            catch
+            {
+                logger.LogWrite("文本框已经为空！");
+            }
         }
     }
+
+    public class Datetime
+    {
+        public string date()
+        {
+            return (DateTime.Now.ToLongDateString());
+        }
+
+        public string time()
+        {
+            return (DateTime.Now.ToLongTimeString());
+        }
+
+    }
+    /// <summary>
+    /// model 
+    /// </summary>
+    public class Note
+    {
+        public long NoteId { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public string Text { get; set; }      
+    }
+  
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("MyDBContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<MyDbContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+
 }
---
+++ CreateGUID/LogWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace CreateGUID
+{
+    public class LogWriter
+    {
+        private string path = string.Empty;
+
+        public LogWriter(string logMessage)
+        {
+            LogWrite(logMessage);
+            EnsureLogFile();
+        }
+
+        public void EnsureLogFile()
+        {
+            path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (!Directory.Exists(path + "\\Log"))
+                Directory.CreateDirectory(path + "\\Log");
+        }
+
+        private string GetLogFileName()
+        {
+            path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var fileName = path + "\\Log" + "\\" + "Log_" + DateTime.Now.Date.ToString("yyyyMMdd") + ".txt";
+            if (!File.Exists(fileName))
+                File.Create(fileName);
+
+            return fileName;
+        }
+
+        public void LogWrite(string logMessage)
+        {
+            try
+            {
+                using (StreamWriter w = File.AppendText(GetLogFileName()))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                txtWriter.WriteLine("  :{0}", logMessage);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+    }
+}
